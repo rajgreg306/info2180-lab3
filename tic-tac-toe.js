e0e2512959d9dbe81d59c2056bbbb9fc6a21f404
Rajae Gregory <rajgreg306@gmail.com>
2025-10-16 20:06:34 -0500

    Added Winner Conditions

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const game_cells = board.getElementsByTagName("div")
 
+
     for(let i = 0; i < game_cells.length; i++){
         game_cells[i].classList.add("square");
     }
@@ -12,6 +13,32 @@ document.addEventListener("DOMContentLoaded", () => {
     let player = "X";
 
     const game = new Array(9);
+    const status = document.getElementById("status")
+    let gameOver = false;
+
+    const winConditions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
+    gameWinner = () => {
+        for (const [a,b,c] of winConditions){
+            if (game[a] && game[a] === game[b] && game[a] == game[c]){
+
+                status.innerHTML = `Congratulations! ${game[a]} is the Winner`
+                status.classList.add("you-won")
+                gameOver = true
+                return;
+            }
+        }
+    }
+
 
 
     for (let i = 0; i < game_cells.length; i++){
@@ -22,6 +49,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 game_cells[i].classList.add("X")
 
                 game[i] = player;
+
+                gameWinner();
 
                 if(player === "X"){
                     player = "O"
