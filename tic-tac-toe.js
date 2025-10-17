dfd97cacd9295dd68983c1ff6018c878fab9be7e
Rajae Gregory <rajgreg306@gmail.com>
2025-10-16 19:43:45 -0500

    Added Game Functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,6 +8,33 @@ document.addEventListener("DOMContentLoaded", () => {
         game_cells[i].classList.add("square");
     }
 
+
+    let player = "X";
+
+    const game = new Array(9);
+
+
+    for (let i = 0; i < game_cells.length; i++){
+
+        game_cells[i].addEventListener("click", () => {
+            if (!game_cells[i].innerHTML){
+                game_cells[i].innerHTML = player;
+                game_cells[i].classList.add("X")
+
+                game[i] = player;
+
+                if(player === "X"){
+                    player = "O"
+                }
+                else{
+                    player = "X"
+                }
+            }
+        })
+    }
+
+
+
 })
 
 
